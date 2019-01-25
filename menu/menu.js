95f0eec8d146a4f3d9ae4994bfb0434824f76e14
mgrinko <glukym@gmail.com>
2019-01-25 16:15:58 +0200

    2nd menu was added

--- menu/menu.js
+++ menu/menu.js
@@ -15,7 +15,7 @@ Menu.prototype._render = function() {
     <ul class="menu__items-list">
       ${ this._items.map(item => `
 
-        <li class="menu__item">111</li>
+        <li class="menu__item">${ item }</li>
 
       `).join('') }
     </ul>
@@ -26,31 +26,15 @@ Menu.prototype.toggle = function () {
   this._element.classList.toggle('menu--closed');
 };
 
-
-let config = {
+let menu1 = new Menu({
   element: document.querySelector('#menu1'),
   title: 'My 123',
   items: [1, 2, 3]
-};
+});
 
-let menu1 = new Menu(config);
+let menu2 = new Menu({
+  element: document.querySelector('#menu2'),
+  title: 'My sdhfgjdsgfhdf',
+  items: [1, 2, 3, 5, 6]
+});
 
-setTimeout(() => {
-  menu1.toggle();
-}, 1000);
-
-setTimeout(() => {
-  menu1.toggle();
-}, 2000);
-
-setTimeout(() => {
-  menu1.toggle();
-}, 3000);
-
-setTimeout(() => {
-  menu1.toggle();
-}, 4000);
-
-setTimeout(() => {
-  menu1.toggle();
-}, 5000);
