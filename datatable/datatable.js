010d05d49338fb4e7e6cdc09b48dfbcc0a26569f
mgrinko <glukym@gmail.com>
2019-02-08 17:05:50 +0200

    Simple data manipulation structure

--- datatable/datatable.js
+++ datatable/datatable.js
@@ -1,19 +1,39 @@
 class Datatable {
   constructor({ element, items, columnConfig }) {
     this._element = element;
-    this._items = items;
+    this._defaultItems = items;
+    this._items = [];
     this._columnConfig = columnConfig;
 
+    this._sortKey = '';
+    this._sortOrder = '';
+    this._currentQuery = '';
+
     this._render();
+
+    this._element.addEventListener('click', (event) => {
+      const th = event.target.closest('[data-sort-key]');
+
+      if (!th) {
+        return;
+      }
+
+      this._sortKey = th.dataset.sortKey;
+      this._render();
+    });
   }
 
   _renderHeader() {
     return `
       <thead>
         <tr>
-          ${ Object.values(this._columnConfig).map(config => `
+          ${ Object.entries(this._columnConfig).map(([key, config]) => `
 
-            <th>${ config.title }</th>
+            <th 
+              ${ config.isSortable ? `data-sort-key="${key}"` : '' }
+            >
+              ${ config.title }
+            </th>
             
           `).join('') }
         </tr>
@@ -32,6 +52,11 @@ class Datatable {
   }
 
   _render() {
+    this._items = this._defaultItems
+      .filter(() => this._currentQuery)
+      .sort(() => this._sortKey);
+
+
     this._element.innerHTML = `
       <table>
         ${ this._renderHeader() }
